2b4828a05ce6c694fb8f91a75579beecba88e7b8
Juan Manuel Echeverria <factorzero@gmail.com>
2021-12-17 12:21:52 -0300

    heroku

--- ProyectoBlog/settings.py
+++ ProyectoBlog/settings.py
@@ -198,3 +198,18 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
